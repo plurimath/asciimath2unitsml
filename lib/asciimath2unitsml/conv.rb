f9978124149eccc0c71d61a0a9692a5a3ef5f5a0
Nick Nicholas <opoudjis@gmail.com>
2021-02-03 20:18:18 +1100

    init

--- lib/asciimath2unitsml/conv.rb
+++ lib/asciimath2unitsml/conv.rb
@@ -1,31 +1,49 @@
 require "asciimath"
 require "nokogiri"
 require "htmlentities"
+require "yaml"
 
 module Asciimath2UnitsML
   MATHML_NS = "http://www.w3.org/1998/Math/MathML".freeze
   UNITSML_NS = "http://unitsml.nist.gov/2005".freeze
 
-  def self.Asciimath2UnitsML(x)
-    xml = Nokogiri::XML(asciimath2mathml(x))
-    xml.xpath(".//m:mtext", "m" => MATHML_NS).each do |x|
-      next unless %r{^unitsml\(.+\)$}.match(x.text)
-      x.replace(unitsml(x.text.sub(%r{^unitsml\((.+)\)$}m, "\\1")))
+  class Conv
+    def initialize
+      @prefixes = read_yaml("../unitsdb/prefixes.yaml")
+      @quantities = YAML.load_file(File.join(File.join(File.dirname(__FILE__),
+                                                       "../unitsdb/quantities.yaml")))
+      @units = read_yaml("../unitsdb/units.yaml")
     end
-    xml.to_xml
-  end
 
-  def self.UnitsML2MathML(x)
-    x
-  end
+    def read_yaml(path)
+      yaml = YAML.load_file(File.join(File.join(File.dirname(__FILE__), path)))
+      yaml.each_with_object({}) do |(k, v), m|
+        m[v["symbol"]] = v
+        m[v["symbol"]]["id"] = k
+      end
+    end
 
-  def self.asciimath2mathml(x)
-    AsciiMath::MathMLBuilder.new(:msword => true).append_expression(
-      AsciiMath.parse(HTMLEntities.new.decode(x)).ast).to_s.
-        gsub(/<math>/, "<math xmlns='#{MATHML_NS}'>")
-  end
+    def Asciimath2UnitsML(x)
+      xml = Nokogiri::XML(asciimath2mathml(x))
+      xml.xpath(".//m:mtext", "m" => MATHML_NS).each do |x|
+        next unless %r{^unitsml\(.+\)$}.match(x.text)
+        x.replace(unitsml(x.text.sub(%r{^unitsml\((.+)\)$}m, "\\1")))
+      end
+      xml.to_xml
+    end
 
-  def self.unitsml(x)
-    "<unitsml xmlns='#{UNITSML_NS}'>#{x}</unitsml>"
+    def UnitsML2MathML(x)
+      x
+    end
+
+    def asciimath2mathml(x)
+      AsciiMath::MathMLBuilder.new(:msword => true).append_expression(
+        AsciiMath.parse(HTMLEntities.new.decode(x)).ast).to_s.
+      gsub(/<math>/, "<math xmlns='#{MATHML_NS}'>")
+    end
+
+    def unitsml(x)
+      "<unitsml xmlns='#{UNITSML_NS}'>#{x}</unitsml>"
+    end
   end
 end
--- spec/conv_spec.rb
+++ spec/conv_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 RSpec.describe Asciimath2UnitsML do
   it "converts an AsciiMath string to MathML + UnitsML" do
-    expect(xmlpp(Asciimath2UnitsML.Asciimath2UnitsML(<<~INPUT))).to be_equivalent_to xmlpp(<<~OUTPUT)
+    expect(xmlpp(Asciimath2UnitsML::Conv.new().Asciimath2UnitsML(<<~INPUT))).to be_equivalent_to xmlpp(<<~OUTPUT)
     32 + 5 xx 7 "unitsml(g)"
     INPUT
     <math xmlns='http://www.w3.org/1998/Math/MathML'>
@@ -17,7 +17,7 @@ RSpec.describe Asciimath2UnitsML do
   end
 
   it "converts MathML + UnitsML to pure MathML" do
-    expect(xmlpp(Asciimath2UnitsML.UnitsML2MathML(<<~INPUT))).to be_equivalent_to xmlpp(<<~OUTPUT)
+    expect(xmlpp(Asciimath2UnitsML::Conv.new().UnitsML2MathML(<<~INPUT))).to be_equivalent_to xmlpp(<<~OUTPUT)
         <math xmlns='http://www.w3.org/1998/Math/MathML'>
   <mn>32</mn>
   <mo>+</mo>
